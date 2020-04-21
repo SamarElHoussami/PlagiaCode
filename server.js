3d76c337e1dfe414a8962c64dc4d438c054b2722
Samar El-Houssami <samarhoussami@gmail.com>
2020-04-20 22:43:01 -0400

    updated server

--- server.js
+++ server.js
@@ -13,17 +13,6 @@ var cors = require('cors')
 
 app.use(cors()) // Use this after the variable declaration
 
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('client/build'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
