8fcf5e289f7a008db017afbc443da196372d9fef
Samar El-Houssami <samarhoussami@gmail.com>
2020-04-21 02:09:49 -0400

    added tests that hopefully work

--- server.js
+++ server.js
@@ -49,13 +49,13 @@ app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
-
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.get('/port', function (req, res) {
  return res.send(port);
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !!`));
