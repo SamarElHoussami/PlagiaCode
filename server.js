24a684edfc8632eb94b4cf0229c17f36073cc1ea
Samar El-Houssami <samarhoussami@gmail.com>
2020-04-21 00:08:00 -0400

    followed tutorial

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const users = require("./routes/api/users");
 const courses = require("./routes/api/courses");
 const postings = require("./routes/api/postings");
 var multer = require('multer');
-
+const favicon = require('express-favicon');
+const path = require('path');
 const app = express();
 
 var cors = require('cors')
@@ -42,6 +43,17 @@ app.use("/api/users", users);
 app.use("/api/courses", courses);
 app.use("/api/postings", postings);
 
+//for deployment
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !!`));
