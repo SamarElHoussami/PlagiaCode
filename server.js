9d07bf7b26e7795b53d9881ba9cfe5346917315f
Samar El-Houssami <samarhoussami@gmail.com>
2020-04-22 01:28:07 -0400

    fixing not uploading problem

--- server.js
+++ server.js
@@ -52,11 +52,6 @@ app.get('/ping', function (req, res) {
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 
 if(port !== 5000) {
-  app.get('/public/assignments/:assign', (req, res) => {
-    const assignName = req.params.assign.substr(13);
-      res.sendFile(path.join(__dirname, "client", "public", "assignments", assignName)); //for deployed
-  });
-
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
